720c66a0f885f094888c5ae6a32a0a9781ac5e00
bilal <bilal94@students.highline.edu>
2022-01-29 04:06:54 -0800

    Pro1

--- project1.cpp
+++ project1.cpp
@@ -2,17 +2,46 @@
 using namespace std;
 int main()
 {
-string noun , place , thing;
-  
-cout<<"Give me a proper noun: ";
-getline(cin,noun);
-cout<<"\nGive me a place: ";
-getline(cin,place);
-cout<<"\nGive me a thing: ";
-getline(cin,thing);
-  
-
-cout<<"\n"<<noun<<" went to the "<<place<<" to get a "<<thing<<".";
-  
+string friendName,name,food,number,adjective,color,animal,yourName;
+string word; //prompt;
+char space;
+//ifstream in;
+int num;
+cout<<"Enter the first of your friend: ";
+cin >> friendName;
+cout << "Enter your name: ";
+cin >> yourName;
+cout << "Enter a food item: ";
+cin >> food;
+cout<<"Enter a number between 100 and 120: ";
+cin >> number;
+cout <<" Please enter an adjective: ";
+cin >>adjective;
+cout <<" Enter a color: ";
+cin >> color;
+cout <<"Please enter an animal: ";
+cin >> animal;
+cout << "Dear friend "; cout << friendName;
+cout << ",:\n";
+cout << "I am so sorry that I am unable to come to your place at this time. \n";
+cout << " First, I ate a rotten ";
+cout << food;
+cout << ", which made me turn ";
+cout << color;
+cout << " and extremely ill. I came down with a fever of ";
+cout << number;
+cout << ". Next my ";
+cout << adjective;
+cout << " pet ";
+cout << animal;
+cout << " must have smelled the remains of the";
+cout << food;
+cout << " i do have assignments.\n";
+cout << " I am currently doing my assignments and hope you will accept my exceuses. \n";
+cout << "Love you friend, \n";
+cout << yourName;
 return 0;
+
+
+
 }
